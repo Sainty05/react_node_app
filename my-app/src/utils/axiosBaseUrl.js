ab74e138405a0045311a42468b09b0d99feb282c
Santosh Rathore <112748726+Sainty05@users.noreply.github.com>
2023-08-10 14:51:46 +0530

    Update axiosBaseUrl.js
    axios production link

--- my-app/src/utils/axiosBaseUrl.js
+++ my-app/src/utils/axiosBaseUrl.js
@@ -1,5 +1,9 @@
 import Axios from 'axios';
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://64d4aadc54554f46ae80c5fa--melodious-taiyaki-60da5a.netlify.app/api' // Replace with your production API URL
+  : 'http://localhost:5000/api/'; // Use local development API URL
+
 const axiosBaseURL = Axios.create({
-    baseURL: 'http://localhost:5000/api/'
+    baseURL: baseURL
 });
 export default axiosBaseURL
